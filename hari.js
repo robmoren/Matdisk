b9d5d1fc40d824fa2d31bf029fdfffd710797666
robmoren <54829728+robmoren@users.noreply.github.com>
2019-09-24 12:40:17 +0700

    Add files via upload

---
+++ hari.js
@@ -0,0 +1,26 @@
+var angka = new Date().getDay()
+
+switch (angka){
+    case 0:
+        console.log("Minggu") ;
+        break ;
+    case 1 :
+        console.log("Senin") ;
+        break ;
+    case 2 :
+        console.log("Selasa") ;
+        break ;
+    case 3 :
+        console.log("rabu") ;
+        break ;
+    case 4 :
+        console.log("Kamsi") ;
+        break ;
+    case 5 :
+        console.log("Jumat") ;
+        break ;
+    case 6 :
+        console.log("Sabtu") ;
+        break ;
+
+}
